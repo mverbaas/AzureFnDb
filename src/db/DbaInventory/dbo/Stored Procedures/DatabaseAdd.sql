6489fc0b0ff0311f5108ca72e552e56b3092b9e8
Mark Verbaas <mverbaas@live.com>
2022-11-13 20:46:25 +0100

    change schema, add loging succes events

--- src/db/DbaInventory/dbo/Stored Procedures/DatabaseAdd.sql
+++ src/db/DbaInventory/dbo/Stored Procedures/DatabaseAdd.sql
@@ -6,6 +6,7 @@ BEGIN
   SET NOCOUNT ON;
 
   DECLARE @InstanceId INT
+    , @ObjectName SYSNAME = OBJECT_NAME(@@PROCID)
   
   BEGIN TRY
     -- Check if instance exists or create if not exists
@@ -23,17 +24,22 @@ BEGIN
     --Insert the databases
     INSERT [dbo].[Database] ([InstanceId], [Name], [CreateDate])
     VALUES (@InstanceId, @Name, DEFAULT)
+
+    DECLARE @EventMessage NVARCHAR(128) = FORMATMESSAGE('Added database %s for %s', @Name, @ServerInstance)
+
+    EXECUTE [Utils].[LogEvent] @Source = @ObjectName
+      , @EventMessage = @EventMessage
+
   END TRY
   BEGIN CATCH
-    DECLARE @ObjectName SYSNAME = OBJECT_NAME(@@PROCID)
-      , @ErrorMessage NVARCHAR(MAX) = ERROR_MESSAGE()
+    DECLARE @ErrorMessage NVARCHAR(MAX) = ERROR_MESSAGE()
 
     IF @@TRANCOUNT > 0
     BEGIN
       ROLLBACK TRAN
     END
 
-    EXECUTE [log].[LogError] @Source = @ObjectName
+    EXECUTE [Utils].[LogError] @Source = @ObjectName
       , @Error = @ErrorMessage;
 
     THROW
--- src/db/DbaInventory/dbo/Stored Procedures/InstanceAdd.sql
+++ src/db/DbaInventory/dbo/Stored Procedures/InstanceAdd.sql
@@ -3,23 +3,30 @@ CREATE PROCEDURE [dbo].[InstanceAdd]
 AS
 BEGIN
   SET NOCOUNT ON;
+
+  DECLARE @ObjectName SYSNAME = OBJECT_NAME(@@PROCID)
   
   BEGIN TRY
     BEGIN TRAN
       INSERT [dbo].[Instance] ([Name], [CreateDate])
       VALUES (@Name, DEFAULT)
+
+      DECLARE @EventMessage NVARCHAR(128) = FORMATMESSAGE('Added ServerInstance %s to [dbo].[Instances]', @Name)
+      
+      EXECUTE [Utils].[LogEvent] @Source = @ObjectName
+        , @EventMessage = @EventMessage
+
     COMMIT TRAN
   END TRY
   BEGIN CATCH
-    DECLARE @ObjectName SYSNAME = OBJECT_NAME(@@PROCID)
-      , @ErrorMessage NVARCHAR(MAX) = ERROR_MESSAGE()
+    DECLARE @ErrorMessage NVARCHAR(MAX) = ERROR_MESSAGE()
 
     IF @@TRANCOUNT > 0
     BEGIN
       ROLLBACK TRAN
     END
 
-    EXECUTE [log].[LogError] @Source = @ObjectName
+    EXECUTE [Utils].[LogError] @Source = @ObjectName
       , @Error = @ErrorMessage;
 
     THROW
